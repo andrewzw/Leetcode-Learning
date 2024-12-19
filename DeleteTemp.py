2b6fc035f01e73d8c33f3aeb3f6a19d7ce5a1da4
andrewzw <yapzhewei2002@gmail.com>
2024-12-19 15:27:13 +1100

    delete temp files script

---
+++ DeleteTemp.py
@@ -0,0 +1,41 @@
+import os
+
+
+def delete_files(folder_path):
+    deleted_files = 0
+    permission_denied = 0
+    error_deleting = 0
+    error_accessing = 0
+    print(f"\nCleaning folder: {folder_path}")
+    try:
+        files = os.listdir(folder_path)
+        for file in files:
+
+            file_path = os.path.join(folder_path, file)
+            try:
+                os.remove(file_path)
+                print(f"Deleted: {file}")
+                deleted_files += 1
+            except PermissionError as e:
+                print(f"Permission denied: {file}: {e}")
+                permission_denied += 1
+            except Exception as e:
+                print(f"Error deleting: {file}: {e}")
+                error_deleting += 1
+    except Exception as e:
+        print(f"Error accessing folder {folder_path}: {str(e)}")
+        error_accessing += 1
+    print(
+        f"\n=============== Logs ===============\nFolder: {folder_path}\nFiles deleted: {deleted_files}\nPermission denied: {permission_denied}\nError deleting: {error_deleting}\nError accessing: {error_accessing}"
+    )
+    print("Finished cleaning folder \n=============== End ===============")
+
+
+def userName(user):
+    folders = [f"C:/Users/{user}/AppData/Local/Temp", "C:/Windows/Temp"]
+    return folders
+
+
+folders = userName("ZW")
+for folder in folders:
+    delete_files(folder)
