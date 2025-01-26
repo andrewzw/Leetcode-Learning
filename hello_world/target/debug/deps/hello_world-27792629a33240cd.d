58452a1913289ab66bac0787f009a94baadb4c16
andrewzw <yapzhewei2002@gmail.com>
2025-01-26 18:13:47 +1100

    rust

--- hello_world/target/debug/deps/hello_world-27792629a33240cd.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ZW\Desktop\Leetcode-Learning\hello_world\target\debug\deps\libhello_world-27792629a33240cd.rmeta: src/main.rs
-
-C:\Users\ZW\Desktop\Leetcode-Learning\hello_world\target\debug\deps\hello_world-27792629a33240cd.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ZW\Desktop\Leetcode-Learning\hello_world\target\debug\hello_world.exe: C:\Users\ZW\Desktop\Leetcode-Learning\hello_world\src\main.rs
